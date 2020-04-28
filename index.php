7ff84214e504e3605958bb7745f68b19e1703f11
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-04-28 09:25:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,12 @@
 <body>
   <?php
   
-  for ($i=0; $i <= 100; $i=$i+2) { 
+  for ($i=0; $i <= 20; $i=$i++) { 
       echo  $i . "<br>";
   }
-
+for ($i=0; $i <= 20 ; $y=$i*$i) { 
+    echo $y;
+}
   ?>
 </body>
 </html>
